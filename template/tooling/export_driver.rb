36801e76f9b26c4465e0306783d55b862d904075
Jack Boespflug <jack.boespflug@kineticdata.com>
2025-05-16 07:45:29 -0500

    KP-8966: Export driver comments

--- template/tooling/export_driver.rb
+++ template/tooling/export_driver.rb
@@ -1,3 +1,20 @@
+# A driver that will convert the configuration information into a format that the export process expects.
+#
+# The export driver utilizes environment variables to provide the necessary configuration information
+# to the export process. Below are the environment variables and what they are used for.
+#
+# Required Environment Variables
+# -------------------------------
+# EXPORT_DOMAIN                 - The name of the domain hosting the space to be exported
+# EXPORT_SPACE_SLUG             - The slug of the space to be exported
+# EXPORT_SPACE_ADMIN_USERNAME   - The username of a space admin user account in the space to be exported
+# EXPORT_SPACE_ADMIN_PASSWORD   - The password for the corresponding space admin user account
+#
+# Optional Environment Variables
+# -------------------------------
+# EXPORT_OAUTH_CLIENT_ID        - The OAuth Client Id if different than the space admin username
+# EXPORT_OAUTH_CLIENT_SECRET    - The OAuth Client Secret for the corresponding OAuth Client Id
+#
 require 'json'
 
 env_errors = []
